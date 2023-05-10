f602b09503d918ae6ac015b1ab233c07e6d42f4b
FabianGenell <fabian.genell@hyperisland.se>
2023-05-10 23:47:54 +0200

    Increased responsivenes

--- app/home/components/Project.jsx
+++ app/home/components/Project.jsx
@@ -15,21 +15,24 @@ import { BiRightArrowAlt } from 'react-icons/bi'
 export default function Project({ project, children }) {
     return <div className={styles.project}>
         <Image src={project.image} width={1920} height={1080} alt='2D Fighting game called HyperBattle' />
-
-        <div className={styles.textbox}>
-            <h3>{project.title}</h3>
-            <p>{project.description}</p>
-        </div>
-        <div className={styles.buttonbox}>
-            <div className={styles.buttonContainer}>
-                {project.live && <a className={styles.button} href={project.live}>VIEW LIVE VERSION</a>}
-                {project.github && <a className={styles.button} href={project.github}> <FaGithubSquare size='20px' /> GITHUB REPO</a>}
+        <div>
+            <div className={styles.textbox}>
+                <h3>{project.title}</h3>
+                <p>{project.description}</p>
             </div>
-            <div className={styles.iconContainer}>
-                {children}
+            <div className={styles.buttonbox}>
+                <div className={styles.buttonContainer}>
+                    {project.live && <a className={styles.button} href={project.live}>VIEW LIVE VERSION</a>}
+                    {project.github && <a className={styles.button} href={project.github}> <FaGithubSquare size='20px' /> GITHUB REPO</a>}
+                </div>
+                <div className={styles.iconContainer}>
+                    {children}
+                </div>
             </div>
+            <a href="" className={styles.moreInfo}>MORE INFO <BiRightArrowAlt size={20} /></a>
+
         </div>
 
-        <a href="" className={styles.moreInfo}>MORE INFO <BiRightArrowAlt size={20} /></a>
+
     </div>
 }
--- app/home/page.js
+++ app/home/page.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import styles from './page.module.scss'
 import Sugarpunch from 'next/font/local'
-import Project from './components/projects'
+import Project from './components/Project'
 
 import { SiJavascript, SiCss3, SiHtml5, SiNodedotjs, SiPostgresql, SiReact, SiTypescript, SiWebgl, SiShopify, SiSass, SiGithub, SiLinkedin } from 'react-icons/si'
 import { MdEmail } from 'react-icons/md'
@@ -87,21 +87,27 @@ export default function Home() {
 
       <section className={styles.about}>
         <h2>about me</h2>
-        <p>My name is Fabian. I’m a 21 year old guy from Sweden. I am currently studying Frontend Development at HYPER ISLAND.</p>
-        <Image src='/img/fabian/italy-lagonegro.png' width={300} height={300} alt="Fabian Genell" />
-        <p>But, I’ve actually been coding since I was 12 when I wanted upgrade my minecraft server so I was forced to dive in to Java.</p>
-        <p>Although I didn’t touch code for a couple of years I’ve found my way back but this time to web development.</p>
-        <Image src='/img/fabian/jamtland.jpg' width={500} height={300} alt="Fabian Genell i  natur" />
-        <div>
-          <strong>Outside of coding I enjoy</strong>
-          <ul>
-            <li>Documentary Film</li>
-            <li>Photography</li>
-            <li>Exercising</li>
-            <li>Muay Thai</li>
-            <li>Sales & Marketing</li>
-            <li>Business</li>
-          </ul>
+        <div className={styles.aboutSection}>
+          <Image src='/img/fabian/italy-lagonegro.png' width={300} height={300} alt="Fabian Genell" />
+          <div>
+            <p>My name is Fabian. I’m a 21 year old guy from Sweden. I am currently studying Frontend Development at HYPER ISLAND.</p>
+            <p>But, I’ve actually been coding since I was 12 when I wanted upgrade my minecraft server so I was forced to dive in to Java.</p>
+            <p>Although I didn’t touch code for a couple of years I’ve found my way back but this time to web development.</p>
+          </div>
+        </div>
+        <div className={styles.aboutSection}>
+          <Image src='/img/fabian/jamtland.jpg' width={576} height={384} alt="Fabian Genell i  natur" />
+          <div>
+            <strong>Outside of coding I enjoy</strong>
+            <ul>
+              <li>Documentary Film</li>
+              <li>Photography</li>
+              <li>Exercising</li>
+              <li>Muay Thai</li>
+              <li>Sales & Marketing</li>
+              <li>Business</li>
+            </ul>
+          </div>
         </div>
 
 
